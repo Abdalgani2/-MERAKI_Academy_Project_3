0d7e049475d558c7d4655c8bd85262a74ab5a976
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-05-15 16:52:01 +0300

    finsh get articles by author

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+// const uuidv4 =require("uuidv4");
+// uuid=uuidv4();
 const port = 5000;
 app.use(express.json());
 const articles = [
@@ -30,6 +32,19 @@ const getAllArticles = (req, res) => {
     res.json(articles);
 };
 app.get("/articles", getAllArticles);
+const getArticlesByAuthor= (req, res) => {
+    const arr=[]
+    const auth = req.query.author;
+    articles.forEach((element) => {
+        if (element.author === auth) {
+            arr.push(element);
+        } ;
+    });
+    res.json(arr);
+};
+app.get("/articles/search_1",getArticlesByAuthor)
+
+console.log("ddddd")
 app.listen(port, () => {
     console.log(`the server run the port ${port}`);
 });
